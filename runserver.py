0931066751568d36f0af6555ef0e40faa9014be1
Alfie Umbhau <aumbhau@gmail.com>
2012-11-20 17:15:59 -0500

    heroku setup

--- runserver.py
+++ runserver.py
@@ -1,2 +1,6 @@
-from LOC import app
-app.run(debug=True)
+from LenderPortal import app
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)

8ae6c850cc223abb91eb8dd7f85b74a8bd02fdb3
Alfie Umbhau <aumbhau@gmail.com>
2012-11-20 17:58:26 -0500

    added csv download

--- LenderPortal/__init__.py
+++ LenderPortal/__init__.py
@@ -1,11 +1,13 @@
 #imports
 from __future__ import with_statement
-from flask import Flask, render_template, request, session, flash, g, redirect, url_for, _app_ctx_stack
+from flask import Flask, Response, render_template, request, session, flash, g, redirect, url_for, _app_ctx_stack
 from contextlib import closing
 import pprint
 import json
 import os
 import uuid
+import csv
+
 # from db import DB
 
 # CONFIG
@@ -22,3 +24,17 @@ app.secret_key = '1-\xf1\xec\x10\x1eN@Zv\x95\x93\xe3\x0c\x96\xf8H&\xd8\xaa\xdc2\
 @app.route('/', methods=['GET', 'POST'])
 def dashboard():
 	return render_template('home.html')
+
+#THIS WORKS
+@app.route('/application.csv', methods=['GET', 'POST'])
+def generate_large_csv():
+    def generate():
+        for row in iter_all_rows():
+            yield ','.join(row) + '\n'
+    return Response(generate(), mimetype='text/csv')
+
+#THIS SORT OF WORKS
+@app.route('/test.csv', methods=['GET', 'POST'])
+def generate_large_csv():
+	data = 'hello'
+	return Response(data, mimetype='text/csv')
